02205b03a9520c4cda0a82764b25631c0f4a4b73
Rakebul Hasan <rakeb04@gmail.com>
2011-11-04 12:01:57 +0100

    forms added

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from mysite.views import hello, current_datetime, hours_ahead
+from mysite.books import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -24,4 +25,7 @@ urlpatterns = patterns('',
     
     #view for hours ahead    
     (r'^time/plus/(\d{1,2})/$', hours_ahead),
+    
+    (r'^search-form/$', views.search_form),
+    (r'^search/$', views.search),
 )
